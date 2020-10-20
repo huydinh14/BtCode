55c8a4f45597d5a19836fe6fabf053ed9ec8d81f
dinh <dinhhuymemrise@gmail.com>
2020-10-20 23:06:03 +0700

    Bt code

---
+++ Project1/Project1/main.cpp
@@ -0,0 +1,355 @@
+﻿#include <iostream>
+
+using namespace std;
+
+/*
+* 7 phần:
+* 1. Khởi báo.
+* 2. Khởi tạo.
+* 3. Tạo nút.
+* 4. Thêm đầu, thêm cuối.
+* 5. Thêm nhiều nút.
+* 6. Xử lý theo yêu cầu.
+* 7. Giải phóng nút.
+*
+* Danh sách liên kết đôi:
+* 1. Thêm/xóa.
+* 2. Thêm đầu, thêm cuối.
+* 3. Thêm vào sau, thêm vào trước => Bài tập về nhà
+* 4. Xóa 1 nút với mã => Bài tập về nhà.
+* 5. Xuất danh sách từ trước ra sau và sau ra trước.
+*
+* Link List Single: Danh sách liên kết đơn.
+* Link List Double: Danh sách liên kết đôi.
+*/
+
+// 1. Khởi báo.
+struct Node
+{
+	int IData;
+	Node* PtrNext;
+	Node* PtrRev;
+};
+
+struct LinkedListDou
+{
+	Node* PtrHead;
+	Node* PtrTail;
+};
+
+//2. Khởi tạo.
+void linkedListDouInit(LinkedListDou&);
+
+//3. Tạo nút.
+Node* linkedListDouCreateNode(int);
+Node* InsertNode(int&);
+
+//4. Thêm đầu, thêm cuối.
+void linkedListDouAddHead(LinkedListDou&, Node*); // Thêm đầu.
+void linkedListDouAddTail(LinkedListDou&, Node*); // Thêm cuối.
+
+//5. Thêm nhiều nút.
+void linkedListDouInput(LinkedListDou&);
+void linkedListDouOutputAfter(LinkedListDou);
+void linkedListDouOutputBefore(LinkedListDou);
+
+//6. Xử lý theo yêu cầu.
+//void linkedListDouOutput(LinkedListDou);
+void linkedListDouBubble(LinkedListDou);
+//Thêm vào sau, thêm vào trước
+void linkedListAddBefore(LinkedListDou&, Node*, int&);
+void linkedListAddAfter(LinkedListDou&, Node*, int&);
+void linkedListAddBeforeMust(LinkedListDou&, Node*, int&);
+void linkedListAddAfterMust(LinkedListDou&, Node*, int&);
+//Xóa 1 nút với mã
+void linkedListDelHead(LinkedListDou&);
+void linkedListDelTail(LinkedListDou&);
+void linkedListDelBefore(LinkedListDou&, Node*);
+void linkedListDelAfter(LinkedListDou&, Node*);
+
+//7. Giải phóng nút.
+void linkedListDouFree(LinkedListDou&);
+
+int main()
+{
+	LinkedListDou l;
+	linkedListDouInput(l);
+	cout << "Danh sach sau khi nhap:" << endl;
+	linkedListDouOutputAfter(l);
+	//linkedListDouOutputBefore(l);
+
+	int x, q;
+	//Xoa dau - xoa cuoi- xoa vi tri bat ky
+	//linkedListDelHead(l);
+	//linkedListDelTail(l);
+	//linkedListDelBefore(l,InsertNode(q));
+	//linkedListDelAfter(l,InsertNode(q));
+	//Sap xep Bubblesort
+	//linkedListDouBubble(l);
+	//Them truoc - them sau
+	//linkedListAddBefore(l,InsertNode(q),x);
+	linkedListAddAfter(l, InsertNode(q), x);
+	//linkedListAddBeforeMust(l,InsertNode(q), x);
+	//linkedListAddAfterMust(l,InsertNode(q),x);
+
+	//cout << "Danh sach sau khi them: ";
+	linkedListDouOutputAfter(l);
+	linkedListDouFree(l);
+	cout << endl;
+	system("pause");
+	return 0;
+}
+
+void linkedListDouInit(LinkedListDou& ld)
+{
+	ld.PtrHead = ld.PtrTail = nullptr;
+}
+
+Node* linkedListDouCreateNode(int IData)
+{
+	Node* PtrNode = new Node;
+	if (!PtrNode)
+	{
+		return nullptr;
+	}
+	PtrNode->IData = IData;
+	PtrNode->PtrNext = nullptr;
+	PtrNode->PtrRev = nullptr;
+	return PtrNode;
+}
+Node* InsertNode(int& q)
+{
+	cout << "Nhap du lieu node can them:";
+	cin >> q;
+	Node* PNode = linkedListDouCreateNode(q);
+	return PNode;
+}
+void linkedListDouAddHead(LinkedListDou& ld, Node* PtrNode)
+{
+	if (!ld.PtrHead)
+	{
+		ld.PtrHead = ld.PtrTail = PtrNode;
+		return;
+	}
+	PtrNode->PtrNext = ld.PtrHead;
+	ld.PtrHead->PtrRev = PtrNode;
+	ld.PtrHead = PtrNode;
+}
+
+void linkedListDouAddTail(LinkedListDou& ld, Node* PtrNode)
+{
+	if (!ld.PtrHead)
+	{
+		ld.PtrHead = ld.PtrTail = PtrNode;
+		return;
+	}
+	ld.PtrTail->PtrNext = PtrNode;
+	PtrNode->PtrRev = ld.PtrTail;
+	ld.PtrTail = PtrNode;
+}
+
+void linkedListDouInput(LinkedListDou& ld)
+{
+	linkedListDouInit(ld);
+
+	int length;
+	cout << "Nhap so luong nut: ";
+	cin >> length;
+
+	for (int i = 0; i < length; i++)
+	{
+		int IData;
+		cout << "Nhap vao nut thu " << i + 1 << ": ";
+		cin >> IData;
+
+		//linkedListDouAddHead(ld, linkedListDouCreateNode(IData));
+		linkedListDouAddTail(ld, linkedListDouCreateNode(IData));
+	}
+}
+
+void linkedListDouOutputAfter(LinkedListDou ld)
+{
+	for (Node* i = ld.PtrHead; i != nullptr; i = i->PtrNext)
+	{
+		cout << i->IData << " ";
+	}
+	cout << endl;
+}
+
+void linkedListDouOutputBefore(LinkedListDou ld)
+{
+	for (Node* i = ld.PtrTail; i != nullptr; i = i->PtrRev)
+	{
+		cout << i->IData << " ";
+	}
+	cout << endl;
+}
+void linkedListDouBubble(LinkedListDou ld)
+{
+	for (Node* i = ld.PtrHead; i != ld.PtrTail; i = i->PtrNext) // i = i->PtrNext <=> i++
+	{
+		for (Node* j = ld.PtrTail; j != i; j = j->PtrRev) // j = j->PtrPrev <=> j--
+		{
+			/*
+			* j <=> j
+			* j->PtrRev <=> j - 1.
+			* j->IData <=> a[j].
+			* j->PtrRev->IData <=> a[j - 1]
+			*/
+			if (j->IData < j->PtrRev->IData)
+			{
+				swap(j->IData, j->PtrRev->IData);
+			}
+		}
+	}
+}
+//Thêm vào trước
+void linkedListAddBefore(LinkedListDou& l, Node* q, int& x)
+{
+	cout << "Nhap du lieu node them(truoc):";
+	cin >> x;
+	Node* p = l.PtrHead->PtrNext;
+	if (l.PtrHead->IData == q->IData)
+	{
+		Node* pTemp = linkedListDouCreateNode(x);
+		linkedListDouAddHead(l, pTemp);
+		return;
+	}
+	while (p->IData != q->IData)
+	{
+		p = p->PtrNext;
+	}
+	Node* PtrNode = linkedListDouCreateNode(x);
+	PtrNode->PtrNext = p;
+	PtrNode->PtrRev = p->PtrRev;
+	p->PtrRev->PtrNext = PtrNode;
+	p->PtrRev = PtrNode;
+}
+void linkedListAddAfter(LinkedListDou& l, Node* q, int& x)
+{
+	cout << "Nhap du lieu node them(truoc):";
+	cin >> x;
+	Node* p = l.PtrHead->PtrNext;
+	while (p->IData != q->IData)
+	{
+		p = p->PtrNext;
+	}
+	Node* PtrNode = linkedListDouCreateNode(x);
+	PtrNode->PtrRev = p;
+	PtrNode->PtrNext = p->PtrNext;
+	p->PtrNext = PtrNode;
+	p->PtrNext->PtrRev = PtrNode;
+	if (l.PtrTail->IData == q->IData)
+	{
+		Node* Temp = linkedListDouCreateNode(x);
+		linkedListDouAddTail(l, Temp);
+	}
+}
+void linkedListAddBeforeMust(LinkedListDou& l, Node* q, int& x)
+{
+	cout << "Nhap du lieu node them(truoc):";
+	cin >> x;
+	Node* p = l.PtrHead->PtrNext;
+	while (p)
+	{
+		if (p->IData == q->IData)
+		{
+			Node* PtrNode = linkedListDouCreateNode(x);
+			PtrNode->PtrNext = p;
+			PtrNode->PtrRev = p->PtrRev;
+			p->PtrRev->PtrNext = PtrNode;
+			p->PtrRev = PtrNode;
+		}
+		p = p->PtrNext;
+	}
+	if (l.PtrHead->IData == q->IData)
+	{
+		Node* PtrNode = linkedListDouCreateNode(x);
+		linkedListDouAddHead(l, PtrNode);
+	}
+}
+//Thêm vào sau
+void linkedListAddAfterMust(LinkedListDou& l, Node* q, int& x)
+{
+	cout << "Nhap du lieu node them(sau):";
+	cin >> x;
+	Node* p = l.PtrHead;
+	while (p)
+	{
+		if (p->IData == q->IData)
+		{
+			Node* PtrNode = linkedListDouCreateNode(x);
+			PtrNode->PtrRev = p;
+			PtrNode->PtrNext = p->PtrNext;
+			p->PtrNext = PtrNode;
+			p->PtrNext->PtrRev = PtrNode;
+		}
+		p = p->PtrNext;
+	}
+	if (l.PtrHead->IData == q->IData)
+	{
+		Node* PtrNode = linkedListDouCreateNode(x);
+		linkedListDouAddTail(l, PtrNode);
+	}
+}
+//Xóa 1 nút 
+//Xoa dau
+void linkedListDelHead(LinkedListDou& l)
+{
+	if (l.PtrHead && l.PtrHead->PtrNext)
+	{
+		l.PtrHead = l.PtrHead->PtrNext;
+	}
+	return;
+}
+//Xoa cuoi
+void linkedListDelTail(LinkedListDou& l)
+{
+	Node* PNode = l.PtrHead;
+	while (PNode->PtrNext->PtrNext)
+	{
+		PNode = PNode->PtrNext;
+	}
+	PNode->PtrNext = nullptr;
+}
+//Xoa node
+void linkedListDelBefore(LinkedListDou& l, Node* q)
+{
+	Node* p = l.PtrHead;
+	while (p)
+	{
+		if (p->IData == q->IData)
+		{
+			Node* Temp = p->PtrRev;
+			Temp->PtrRev->PtrNext = p;
+			p->PtrRev = Temp->PtrRev;
+			delete Temp;
+		}
+		p = p->PtrNext;
+	}
+}
+void linkedListDelAfter(LinkedListDou& l, Node* q)
+{
+	Node* p = l.PtrHead;
+	while (p)
+	{
+		if (p->IData == q->IData)
+		{
+			Node* Temp = p->PtrNext;
+			p->PtrNext = Temp->PtrNext;
+			Temp->PtrNext->PtrRev = p;
+			delete Temp;
+		}
+		p = p->PtrNext;
+	}
+}
+void linkedListDouFree(LinkedListDou& ld)
+{
+	Node* PtrNode;
+	while (ld.PtrHead)
+	{
+		PtrNode = ld.PtrHead;
+		ld.PtrHead = ld.PtrHead->PtrNext;
+		delete PtrNode;
+	}
+}
---
+++ Project2/Project2/main.cpp
@@ -0,0 +1,252 @@
+#include <iostream>
+
+using namespace std;
+void InputArray(int*, int);
+void OutputArray(int*, int);
+//176
+void ListedNegative(int*, int);
+//177
+void ListedXY(int*, int, int, int);
+//178
+void ListedEven(int*, int, int, int);
+//179
+void ListedNumberBigAbsAfter(int*, int);
+//180
+void ListedNumberSmallAbsAfter_And_Opposite(int*, int);
+//181
+void ListedEvenNextEven(int*, int);
+//182
+void ListedNextToOppositeSign(int*, int);
+//183
+int SearchValueMax(int*, int);
+void ListedIndexValueMax(int*, int);
+//184
+bool bCheckPrime(int);
+void ListedIndexPrime(int*, int);
+int main()
+{
+	int n = 5;
+	int x = 3;
+	int y = 5;
+	int *Array = new int[n];
+	InputArray(Array, n);
+	cout << "Danh sach sau khi nhap la:";
+	OutputArray(Array, n);
+	cout << endl << "Danh sach cac so am trong mang:";
+	ListedNegative(Array, n);
+	cout << "\nDanh sach cac gia tri thuoc doan " << "[" << x << ";" << y << "]: ";
+	ListedXY(Array, n, x, y);
+	cout << "\nDanh sach cac gia tri chan thuoc doan " << "[" << x << ";" << y << "]: ";
+	ListedEven(Array, n, x, y);
+	cout << "\nDanh sach cac gia tri lon hon tri tuyet doi cua cac gia tri dung sau no: ";
+	ListedNumberBigAbsAfter(Array, n);
+	cout << "\nDanh sach cac gia tri nho hon tri tuyet doi cua cac gia tri dung sau no \nva lon hon tri tuyet doi cua cac gia tri dung truoc no: ";
+	ListedNumberSmallAbsAfter_And_Opposite(Array, n);
+	cout << "\nDanh sach cac gia tri chan ma ke ben no cung la chan: ";
+	ListedEvenNextEven(Array, n);
+	cout << "\nDanh sach cac gia tri co lan can tri dau: ";
+	ListedNextToOppositeSign(Array, n);
+	cout << "\nVi tri ma gia tri tai do la GTLN: ";
+	ListedIndexValueMax(Array, n);
+	cout << "\nVi tri ma gia tri tai do la So nguyen to: ";
+	ListedIndexPrime(Array, n);
+	cout << endl;
+	system("pause");
+	return 0;
+}
+void InputArray(int* A, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << "A[" << i << "]=";
+		cin >> A[i];
+	}
+}
+void OutputArray(int* A, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << " " << A[i];
+	}
+}
+void ListedNegative(int* A, int n)
+{
+	int iTemp = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (A[i] < 0)
+		{
+			iTemp = 1;
+			cout << A[i] << " ";
+		}
+	}
+	if (!iTemp)
+	{
+		cout << " khong co gia tri am!!!";
+	}
+}
+void ListedXY(int* A, int n, int x, int y)
+{
+	int iTemp = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if ((A[i] >= x) && (A[i] <= y))
+		{
+			iTemp = 1;
+			cout << A[i] << " ";
+		}
+	}
+	if (!iTemp)
+	{
+		cout << "Khong co gia tri nao thuoc doan [" << x << ";" << y << "]";
+	}
+}
+void ListedEven(int* A, int n, int x, int y)
+{
+	int iTemp = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (!(A[i] % 2))
+		{
+			if ((A[i] >= x) && (A[i] <= y))
+			{
+				iTemp = 1;
+				cout << A[i] << " ";
+			}
+		}
+	}
+	if (!iTemp)
+	{
+		cout << "Khong co gia tri nao thuoc doan [" << x << ";" << y << "]";
+	}
+}
+void ListedNumberBigAbsAfter(int* A, int n)
+{
+	int iTemp = 0;
+	for (int i = 0; i < (n - 1); i++)
+	{
+		if ((A[i] > abs(A[i + 1])))
+		{
+			iTemp = 1;
+			cout << A[i] << " ";
+		}
+	}
+	if (!iTemp)
+	{
+		cout << "Khong co gia tri nao !!!";
+	}
+}
+void ListedNumberSmallAbsAfter_And_Opposite(int* A, int n)
+{
+	int iTemp = 0;
+	for (int i = 0; i < (n - 1); i++)
+	{
+		if ((A[i] < abs(A[i + 1])) && (A[i] > abs(A[i - 1])))
+		{
+			iTemp = 1;
+			cout << A[i] << " ";
+		}
+	}
+	if (!iTemp)
+	{
+		cout << "Khong co gia tri nao !!!";
+	}
+}
+void ListedEvenNextEven(int* A, int n)
+{
+	int iTemp = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if ((!(A[i] % 2)) && ((!(A[i + 1] % 2)) || (!(A[i - 1] % 2))))
+		{
+			iTemp = 1;
+			cout << A[i] << " ";
+		}
+	}
+	if (!iTemp)
+	{
+		cout << "Khong co gia tri nao !!!";
+	}
+}
+void ListedNextToOppositeSign(int* A, int n)
+{
+	int iTemp = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (((A[i] * A[i + 1]) < 0) || ((A[i] * A[i - 1]) < 0))
+		{
+			iTemp = 1;
+			cout << A[i] << " ";
+		}
+	}
+	if (!iTemp)
+	{
+		cout << "Khong co gia tri nao !!!";
+	}
+}
+int SearchValueMax(int* A, int n)
+{
+	int max = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (A[i] > max)
+		{
+			max = A[i];
+		}
+	}
+	return max;
+}
+void ListedIndexValueMax(int* A, int n)
+{
+	int iTemp = 0;
+	int Max = SearchValueMax(A, n);
+	for (int i = 0; i < n; i++)
+	{
+		if (A[i] == Max)
+		{
+			iTemp = 1;
+			cout << i << " ";
+		}
+	}
+	if (!iTemp)
+	{
+		cout << "Khong co gia tri nao !!!";
+	}
+}
+bool bCheckPrime(int x)
+{
+	if (x < 2)
+	{
+		return false;
+	}
+	if (!(x % 2))
+	{
+		return false;
+	}
+	for (int i = 3; i < sqrt(x); i = i + 2)
+	{
+		if (!(x % i))
+		{
+			return false;
+		}
+	}
+	return true;
+}
+void ListedIndexPrime(int* A, int n)
+{
+	bool check;
+	int iTemp = 0;
+	for (int i = 0; i < n; i++)
+	{
+		check = bCheckPrime(A[i]);
+		if (check)
+		{
+			iTemp = 1;
+			cout << i << " ";
+		}
+	}
+	if (!iTemp)
+	{
+		cout << "Khong co gia tri nao !!!";
+	}
+}
